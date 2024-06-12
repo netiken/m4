db9e1f105569bedb9718bd75287fdc71cd988dbb
lichenni <chenningli2019@gmail.com>
2024-06-12 00:16:08 -0400

    gen data

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ ps aux | head -1; ps aux | grep ^lichenni| sort -rnk 4 | more
 
 tensorboard --logdir /data2/lichenni/output_cc --port 8009 --bind_all
 
-git add -A . ; git commit -m "For now, it lacks the functions to parse transformer texts. Additionally, it requires traces from the real run, which are then parsed via the PyTorch converter."; git push
+git add -A . ; git commit -m "gen data"; git push
 
 >/dev/null
 
