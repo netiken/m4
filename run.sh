dd450ed510e2f342b6717800efc388519bb6cf47
lichenni <chenningli2019@gmail.com>
2024-07-05 23:00:15 -0400

    new traffic

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ ps aux | head -1; ps aux | grep ^lichenni| sort -rnk 4 | more
 
 tensorboard --logdir /data2/lichenni/output_perflow/ --port 8009 --bind_all
 
-git add -A . ; git commit -m "add close-loop gt"; git push
+git add -A . ; git commit -m "new traffic"; git push
 
 >/dev/null
 
