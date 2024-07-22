cbfb2b1db817af86ef5009fa31ce1606158246bf
lichenni <chenningli2019@gmail.com>
2024-07-22 17:52:04 -0400

    finish chaotic test

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ ps aux | head -1; ps aux | grep ^lichenni| sort -rnk 4 | more
 
 tensorboard --logdir /data2/lichenni/output_perflow/ --port 8009 --bind_all
 
-git add -A . ; git commit -m "wrap up res"; git push
+git add -A . ; git commit -m "test chaotic scenarios"; git push
 
 >/dev/null
 
