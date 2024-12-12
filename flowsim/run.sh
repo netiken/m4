7e5e8fb5f4f2343e235edb4eda4804d3d76582bb
lichenni <chenningli2019@gmail.com>
2024-12-12 13:20:25 -0500

    pushing changes

--- flowsim/run.sh
+++ flowsim/run.sh
@@ -1,1 +1,1 @@
-./main 88/ns3/fat.npy 88/ns3/fsize.npy 88/ns3/topology.txt 88/ns3/flow_to_path.txt out.npy
+valgrind ./build/flowsim 88/ns3/fat.npy 88/ns3/fsize.npy 88/ns3/topology.txt 88/ns3/flow_to_path.txt out.npy
