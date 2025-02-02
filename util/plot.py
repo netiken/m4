4b8b15e89310b07018ec027e00d656c2b8193da9
lichenni <chenningli2019@gmail.com>
2025-02-02 01:29:07 -0500

    clean code

--- util/plot.py
+++ util/plot.py
@@ -699,29 +699,3 @@ def plot_box(
     # Save or show the plot
     if file_name:
         plt.savefig(file_name, bbox_inches="tight", pad_inches=0)
-
-
-def create_logger(log_name):
-    logging.basicConfig(
-        # format="%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-        # format="%(asctime)s|%(levelname)s| %(processName)s [%(filename)s:%(lineno)d] %(message)s",
-        format="%(asctime)s|%(filename)s:%(lineno)d|%(message)s",
-        # datefmt="%Y-%m-%d:%H:%M:%S",
-        datefmt="%m-%d:%H:%M:%S",
-        level=logging.INFO,
-        handlers=[logging.FileHandler(log_name, mode="a"), logging.StreamHandler()],
-    )
-    for handler in logging.root.handlers:
-        handler.addFilter(fileFilter())
-
-
-def fix_seed(seed):
-    # os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
-    np.random.seed(seed)
-    random.seed(seed)
-    # torch.use_deterministic_algorithms(True)
